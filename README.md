decf9a767cf6ee3e78a760b156aa2408bf40826d
Aml Fakhri <amlfakhry13@gmail.com>
2022-04-07 11:25:31 +0200

    add main structure for base project.

---
+++ README.md
@@ -0,0 +1,1 @@
+# web_advanced_ewd_egypt
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hallow world');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running now at http://localhost:${port}`);
+});
