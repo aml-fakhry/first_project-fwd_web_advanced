c904d031d7ab190ea6c350c4d46766963f5303ad
Aml Fakhri <amlfakhry13@gmail.com>
2022-04-09 15:56:30 +0200

    add some vaildations.

--- src/data/image-processing.data.ts
+++ src/data/image-processing.data.ts
@@ -1,15 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
+
 import { appRootDir } from './../app';
 
 export class imageProcessDataAccess {
   static async getMetaData(filename: string, width: number, height: number) {
     try {
-      await sharp(appRootDir + `/assets/full/${filename}.jpg`)
-        .withMetadata()
-        .resize(width, height /* , { withoutEnlargement: true } */)
-        .toFile(path.join(appRootDir + `/assets/thumb/thumb_` + Date.now() + '.jpg'));
+      const imageExistence = fs.existsSync(path.join(appRootDir + `/assets/thumb/thumb_` + filename + '.jpg'));
+      if (!imageExistence) {
+        await sharp(appRootDir + `/assets/full/${filename}.jpg`)
+          .withMetadata()
+          .resize(width, height /* , { withoutEnlargement: true } */)
+          .toFile(path.join(appRootDir + `/assets/thumb/thumb_` + filename + '.jpg'));
+      } else {
+        console.log('exist before');
+      }
     } catch (error) {
       console.log(error);
     }
